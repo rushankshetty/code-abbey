cb8fb07b307360e24961bca3fb2b8f545f6e8e9e
rushankshetty <shettyrushank@gmail.com>
2015-04-28 20:57:03 +0530

    solved new puzzle

---
+++ minOfTwo/minOfTwo.py
@@ -0,0 +1,19 @@
+# create an empty list
+inputList = []
+
+# open the file containing the input
+with open('input.txt') as f:
+	for line in f:
+		for word in line.split():
+			# add elements to the empty list
+			inputList.append(word)
+
+# remove the first or 0th element of the list 
+# and store it in a variable
+n = inputList.pop(0)
+
+# start adding two numbers at a time
+while inputList:
+	x = inputList.pop(0)
+	y = inputList.pop(0)
+	print min(int(x), int(y))
