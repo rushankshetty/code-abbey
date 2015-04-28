f0adbf3b32c4c2d2842b664feba5c3ca515233fc
rushankshetty <shettyrushank@gmail.com>
2015-04-28 18:26:43 +0530

    added some comments

--- sumAplusB/sumAplusB.py
+++ sumAplusB/sumAplusB.py
@@ -1,8 +1,12 @@
+# create an empty list
 inputList = []
 
+# open the file containing the input
 with open('input.txt') as f:
 	for line in f:
 		for word in line.split():
+			# add elements to the empty list
 			inputList.append(word)
 
+# print the output
 print "Sum of two numbers is: ", sum(int (i) for i in inputList)
--- sumInLoop/sumInLoop.py
+++ sumInLoop/sumInLoop.py
@@ -1,10 +1,16 @@
+# create an empty list
 inputList = []
 
+# open the file containing the input
 with open('input.txt') as f:
 	for line in f:
 		for word in line.split():
+			# add elements to the empty list
 			inputList.append(word)
 
+# remove the first or 0th element of the list 
+# and store it in a variable
 n = inputList.pop(0)
 
+# print the output
 print "The sum of", n, "values is:", sum(int (i) for i in inputList)
