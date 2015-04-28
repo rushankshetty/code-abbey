2175657b0dd342d1a5051727af9fdb44b5746cc5
Rushank J Shetty <shettyrushank@gmail.com>
2015-04-28 18:15:41 +0530

    Delete sumAplusB.py

--- sum a + b/sumAplusB.py
+++
@@ -1,8 +0,0 @@
-inputList = []
-
-with open('input.txt') as f:
-	for line in f:
-		for word in line.split():
-			inputList.append(word)
-
-print "Sum of two numbers is: ", sum(int (i) for i in inputList)
